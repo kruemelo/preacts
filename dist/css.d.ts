3d8f573cc88cc851cc530cb6bf2df9dc1812cdcf
kruemelo <martin.kuehnel@qintecs.de>
2024-01-28 22:29:32 +0100

    update css.d.ts

--- dist/css.d.ts
+++ dist/css.d.ts
@@ -1,8 +1,8 @@
 export declare const css: {
-    (strings: any, ...values: any[]): string;
+    (strings: string[], ...values: any): string;
     /**
      * Apply the css to the document
      * @param {string} rules css style rules
      */
-    render(rules: any): void;
+    render(rules: string): void;
 };
--- src/css.ts
+++ src/css.ts
@@ -1,11 +1,11 @@
 // a css template literal function just for the sake of ide formatter tools 
-export const css = (strings, ...values) => String.raw({ raw: strings }, ...values);
+export const css = (strings: string[], ...values:any) => String.raw({ raw: strings }, ...values);
 
 /**
  * Apply the css to the document
  * @param {string} rules css style rules
  */
-css.render = (rules) => {
+css.render = (rules:string) => {
   // https://web.dev/articles/constructable-stylesheets
   const constructedStyleSheet = new CSSStyleSheet();
 
