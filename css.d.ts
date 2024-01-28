db2e4c77849551b384ba53099d2a765f2ada2779
kruemelo <martin.kuehnel@qintecs.de>
2024-01-28 19:15:23 +0100

    peerDependenciesMeta

--- css.d.ts
+++
@@ -1,8 +0,0 @@
-export function css(strings: any, ...values: any[]): string;
-export namespace css {
-    /**
-     * Apply the css to the document
-     * @param {string} rules css style rules
-     */
-    function render(rules: string): void;
-}
---
+++ dist/preacts.d.ts
@@ -0,0 +1,8 @@
+import { useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from "preact/hooks";
+import htm from "htm";
+import { Signal, batch, computed, effect, signal, untracked, useComputed, useSignal, useSignalEffect } from "@preact/signals";
+import { Component, h, html, render } from "htm/preact";
+import { css } from "./css.js";
+import register from 'preact-custom-element';
+import * as wouter from "wouter-preact";
+export { htm, useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState, Signal, batch, computed, effect, signal, untracked, useComputed, useSignal, useSignalEffect, Component, h, html, render, register, css, wouter, };
