8a93e61f84a6de06369e1d4189cd03341c241a42
kruemelo <martin.kuehnel@qintecs.de>
2024-02-05 22:45:44 +0100

    fix types

--- dist/preacts.d.ts
+++ dist/preacts.d.ts
@@ -1,30 +1,31 @@
-import * as preact from "preact";
-import { useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from "preact/hooks";
-import htm from "htm";
-import { Signal, batch, computed, effect, signal, untracked, useComputed, useSignal, useSignalEffect } from "@preact/signals";
-import { Component, h, html, render } from "htm/preact";
+export * from "preact";
+export { CreateHandle, Dispatch, EffectCallback, Inputs, MutableRef, Reducer, Ref, StateUpdater, useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState, } from "preact/hooks";
+export * as htm from "htm";
+export * from "@preact/signals";
+export { Component, h, html, render, } from "htm/preact";
 import * as CSS from "./css";
 declare const css: {
     (strings: TemplateStringsArray, ...values: any): string;
     render(rules: string): void;
     classNames(someClassNames: CSS.ClassNames): string;
 };
-import register from 'preact-custom-element';
+export { css, CSS, };
+export * as register from 'preact-custom-element';
 import * as _wouter from "wouter-preact";
 import * as _wouterMemoryLocation from "wouter-preact/memory-location";
 import * as _wouterBrowserLocation from "wouter-preact/use-browser-location";
 import * as _wouterHashLocation from "wouter-preact/use-hash-location";
-declare const wouter: {
-    Route<T extends _wouter.DefaultParams = undefined, RoutePath extends string = string>(props: _wouter.RouteProps<T, RoutePath>): preact.VNode<any>;
+export declare const wouter: {
+    Route<T extends _wouter.DefaultParams = undefined, RoutePath extends string = string>(props: _wouter.RouteProps<T, RoutePath>): import("preact").VNode<any>;
     Redirect<H extends _wouter.BaseLocationHook = _wouterBrowserLocation.BrowserLocationHook>(props: _wouter.RedirectProps<H>, context?: any): null;
-    Link<H_1 extends _wouter.BaseLocationHook = _wouterBrowserLocation.BrowserLocationHook>(props: _wouter.LinkProps<H_1>, context?: any): preact.VNode<any>;
+    Link<H_1 extends _wouter.BaseLocationHook = _wouterBrowserLocation.BrowserLocationHook>(props: _wouter.LinkProps<H_1>, context?: any): import("preact").VNode<any>;
     useRouter(): _wouter.RouterObject;
     useRoute<T_1 extends _wouter.DefaultParams = undefined, RoutePath_1 extends string = string>(pattern: RoutePath_1): _wouter.Match<T_1 extends _wouter.DefaultParams ? T_1 : import("regexparam").RouteParams<RoutePath_1>>;
     useLocation<H_2 extends _wouter.BaseLocationHook = _wouterBrowserLocation.BrowserLocationHook>(): ReturnType<H_2>;
     useSearch<H_3 extends _wouter.BaseSearchHook = _wouterBrowserLocation.BrowserSearchHook>(): ReturnType<H_3>;
     useParams<T_2 = undefined>(): T_2 extends string ? import("regexparam").RouteParams<T_2> : T_2 extends undefined ? _wouter.DefaultParams : T_2;
-    Switch: preact.FunctionComponent<_wouter.SwitchProps>;
-    Router: preact.FunctionComponent<_wouter.RouterProps>;
+    Switch: import("preact").FunctionComponent<_wouter.SwitchProps>;
+    Router: import("preact").FunctionComponent<_wouter.RouterProps>;
     navigate<S = any>(to: string, options?: {
         state: S;
     }): void;
@@ -48,4 +49,3 @@ declare const wouter: {
         record: true;
     }): _wouterMemoryLocation.HookReturnValue & _wouterMemoryLocation.StubHistory;
 };
-export { preact, htm, useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState, Signal, batch, computed, effect, signal, untracked, useComputed, useSignal, useSignalEffect, Component, h, html, render, register, css, CSS, wouter, };
--- src/preacts.ts
+++ src/preacts.ts
@@ -1,6 +1,13 @@
-import * as preact from "preact";
-
-import {
+export * from "preact";
+export { 
+  CreateHandle,
+  Dispatch,
+  EffectCallback,
+  Inputs,
+  MutableRef,
+  Reducer,
+  Ref,
+  StateUpdater,
   useCallback,
   useContext,
   useDebugValue,
@@ -15,96 +22,37 @@ import {
   useState,
 } from "preact/hooks";
 
-import htm from "htm";
+export * as htm from "htm";
 
-import {
-  Signal,
-  batch,
-  computed,
-  effect,
-  signal,
-  untracked,
-  useComputed,
-  useSignal,
-  useSignalEffect,
-} from "@preact/signals";
+export * from "@preact/signals";
 
-// const html = htm.bind(h);
-// import { html } from "htm/preact";
-import {
+export {
   Component,
   h,
   html,
   render,
-  // useCallback,
-  // useContext,
-  // useDebugValue,
-  // useEffect,
-  // useErrorBoundary,
-  // useId,
-  // useImperativeHandle,
-  // useLayoutEffect,
-  // useMemo,
-  // useReducer,
-  // useRef,
-  // useState,
 } from "htm/preact";
 
 import * as CSS from "./css";
 const css = CSS.css;
 
-import register from 'preact-custom-element';
+export {
+  css,
+  CSS,
+};
+
+export * as register from 'preact-custom-element';
 
 // https://github.com/molefrog/wouter
 import * as _wouter from "wouter-preact";
 
-import * as _wouterMemoryLocation from "wouter-preact/memory-location"
-import * as _wouterBrowserLocation from "wouter-preact/use-browser-location"
-import * as _wouterHashLocation from "wouter-preact/use-hash-location"
+import * as _wouterMemoryLocation from "wouter-preact/memory-location";
+import * as _wouterBrowserLocation from "wouter-preact/use-browser-location";
+import * as _wouterHashLocation from "wouter-preact/use-hash-location";
 
-const wouter = {
+export const wouter = {
   ..._wouterMemoryLocation,
   ..._wouterBrowserLocation,
   ..._wouterHashLocation,
   ..._wouter,
-};
-
-export {
-  preact,
-  htm,
-  // preact/hooks
-  useCallback,
-  useContext,
-  useDebugValue,
-  useEffect,
-  useErrorBoundary,
-  useId,
-  useImperativeHandle,
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
-  // @preact/signals
-  Signal,
-  batch,
-  computed,
-  effect,
-  signal,
-  untracked,
-  useComputed,
-  useSignal,
-  useSignalEffect,
-  // htm/preact
-  Component,
-  h,
-  html,
-  render,
-  // preact-custom-element
-  register,
-  // css render, classNames
-  css,
-  CSS,
-  // wouter
-  wouter,
 };
